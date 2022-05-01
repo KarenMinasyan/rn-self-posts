ecafa4725cdeb2b20d2a800c84169ebf8977c48c
Karen Minasyan <kminasyantricking@gmail.com>
2022-05-01 13:56:54 +0400

    add bottom navigation tabs

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,11 +1,15 @@
-import {createAppContainer} from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack'
-import MainScreen from "../screens/MainScreen";
-import PostScreen from "../screens/PostScreen";
-import {THEME} from "../helpers/constants";
-import {Platform} from "react-native";
+import React from 'react'
+import { Platform } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { Ionicons } from '@expo/vector-icons'
+import MainScreen from '../screens/MainScreen'
+import PostScreen from '../screens/PostScreen'
+import BookedScreen from '../screens/BookedScreen'
+import { THEME } from '../helpers/constants'
 
-const {MAIN_COLOR, LIGHT_COLOR} = THEME
+const { MAIN_COLOR, LIGHT_COLOR } = THEME
 
 const PostNavigator = createStackNavigator({
 	Main: MainScreen,
@@ -22,4 +26,37 @@ const PostNavigator = createStackNavigator({
 	}
 })
 
-export const AppNavigation = createAppContainer(PostNavigator)
+const BookedNavigator = createStackNavigator({
+	Booked: BookedScreen,
+	Post: PostScreen
+},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: Platform.OS === 'android' ? MAIN_COLOR : LIGHT_COLOR
+			},
+			headerTintColor: Platform.OS === 'android' ? LIGHT_COLOR : MAIN_COLOR
+		}
+	}
+)
+
+const BottomNavigator = createBottomTabNavigator({
+	Post: {
+		screen: PostNavigator,
+		navigationOptions: {
+			tabBarIcon: info => <Ionicons name='ios-albums' size={25} color={info.tintColor} />
+		}
+	},
+	Booked: {
+		screen: BookedNavigator,
+		navigationOptions: {
+			tabBarIcon: info => <Ionicons name='ios-star' size={25} color={info.tintColor} />
+		}
+	}
+}, {
+	tabBarOptions: {
+		activeTintColor: MAIN_COLOR
+	}
+})
+
+export const AppNavigation = createAppContainer(BottomNavigator)
